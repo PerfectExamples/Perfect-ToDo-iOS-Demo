50787305f6a37bed0ebd05dc65f5f8f9bb0460b8
Ryan Collins <ryan@ryanmcollins.com>
2017-01-18 13:24:35 -0500

    Staged View Controllers
    Signed-off-by: Ryan Collins <ryan@ryanmcollins.com>

---
+++ Perfect-ToDo-iOS/Perfect-ToDo-iOS/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Perfect-ToDo-iOS
+//
+//  Created by Ryan Collins on 1/14/17.
+//  Copyright © 2017 Ryan Collins. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        DataService.instance.load()
+    }
+
+
+}
