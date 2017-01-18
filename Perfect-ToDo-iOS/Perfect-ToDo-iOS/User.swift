36fdbb844a59c796e79255ab399875a7a8a1eb29
Ryan Collins <ryan@ryanmcollins.com>
2017-01-18 13:24:15 -0500

    Added a User model
    Signed-off-by: Ryan Collins <ryan@ryanmcollins.com>

---
+++ Perfect-ToDo-iOS/Perfect-ToDo-iOS/User.swift
@@ -0,0 +1,30 @@
+//
+//  User.swift
+//  Perfect-ToDo-iOS
+//
+//  Created by Ryan Collins on 1/18/17.
+//  Copyright © 2017 Ryan Collins. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    private var _username: String
+    private var _password: String
+    private var _currentToken: String?
+    
+    public var currentToken: String? {
+        get {
+            return _currentToken
+        }
+    }
+    
+    init(user: String, pass: String) {
+        _username = user
+        _password = pass
+    }
+    
+    func authenticate() {
+        
+    }
+}
