5212719373b6fa43dcfd3f8763f2ae4de468ca13
Ryan Collins <ry@rycollins.com>
2018-01-30 15:13:49 -0500

    Update Package to Swift 4

--- Perfect-ToDo-API/Package.swift
+++ Perfect-ToDo-API/Package.swift
@@ -1,23 +1,38 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "ToDO-Backend",
-    targets: [
-        Target(
-            name: "ToDo-API",
-            dependencies: [
-                .Target(name: "ToDoModel")
-            ]
-        ),
-        Target(
-            name: "ToDoModel",
-            dependencies: []
+    products: [
+        .executable(
+            name: "ToDoAPI",
+            targets: ["ToDoAPI"]
         )
     ],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
-        .Package(url: "https://github.com/SwiftORM/MySQL-StORM.git", majorVersion: 3),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Turnstile-MySQL.git", majorVersion: 3),
-        .Package(url: "https://github.com/rymcol/SwiftSQL.git", majorVersion: 0),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", .upToNextMinor(from: "3.0.3")),
+        .package(url: "https://github.com/SwiftORM/MySQL-StORM.git", .upToNextMinor(from: "3.1.1")),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-Turnstile-MySQL.git", .upToNextMinor(from: "3.0.0")),
+        .package(url: "https://github.com/rymcol/SwiftSQL.git", .upToNextMinor(from: "0.2.2")),
+    ],
+    targets: [
+        .target(
+            name: "ToDoAPI",
+            dependencies: [
+                .target(name: "ToDoModel"),
+                "PerfectHTTPServer",
+                "MySQLStORM",
+                "PerfectTurnstileMySQL",
+                "SwiftSQL"
+            ]
+        ),
+        .target(
+            name: "ToDoModel",
+            dependencies: [
+                "SwiftSQL",
+                "MySQLStORM",
+                "PerfectTurnstileMySQL"
+            ]
+        )
     ]
 )
